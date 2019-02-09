9d57c3d67323586e1847ad5eeb553f8193005bde
Bartlomiej Kolodziej <bartlomiej.kolodziej@comarch.pl>
2019-02-09 19:03:53 +0100

    init

--- src/Main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var LoadBalancer_1 = require("./LoadBalancer");
-var loadBalancer = LoadBalancer_1["default"].getInstance();
-loadBalancer.addDatabase('3001');
-loadBalancer.addDatabase('3000');
-loadBalancer.sendQuery('dfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjasdfkjas');
-loadBalancer.sendQuery('1111232131412310784-034444444444444444444444444444444444175-273523=75-847-325784-72354873857');
